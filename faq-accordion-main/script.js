8592c263dfba586ad042185269f3d8af59bd2778
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-06-25 20:48:43 +0200

    script

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -6,7 +6,7 @@ questions.forEach(function (v, i) {
   v.addEventListener("click", function () {
     console.log(current);
     if (current != null) {
-      p[current].outerHTML.replace("class='show'", "");
+      p[current].setAttribute("class", "noShow");
 
       img[current].setAttribute("src", "./assets/images/icon-plus.svg");
     }
@@ -14,9 +14,9 @@ questions.forEach(function (v, i) {
     console.log(p[i].outerHTML.includes("show"));
     if (p[i].outerHTML.includes("show")) {
       img[i].setAttribute("src", "./assets/images/icon-minus.svg");
-      current = i;
     } else {
       img[i].setAttribute("src", "./assets/images/icon-plus.svg");
     }
+    current = i;
   });
 });
